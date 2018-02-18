10817cc5f70cd80edaff747b0c8e5df129becdb2
KNG <34425597+ECHibiki@users.noreply.github.com>
2018-02-17 20:13:03 -0500

    Works without dependencies

--- tweet.php
+++ tweet.php
@@ -70,9 +70,9 @@ function generateMediaSingatureAPPEND($request, $message, $params, $secrets){
 	$request_method = strtoupper($request["request_method"]);
 	$base_url = urlEncodeCustom($request["base_url"]);
 	//$message["media_data"] = 123;
-	$paramter_string = urlEncodeCustom("command=" . $message["command"] . "&media=" . $message["media"] . "&media_id="  . $message["media_id"] . "&oauth_consumer_key=" . $params["oauth_consumer_key"]
+	$paramter_string = urlEncodeCustom("command=" . $message["command"] . "&media=" . urlEncodeCustom($message["media"]) . "&media_id="  .  $message["media_id"] . "&oauth_consumer_key=" . $params["oauth_consumer_key"]
 	 . "&oauth_nonce=" . $params["oauth_nonce"] . "&oauth_signature_method=" . $params["oauth_signature_method"] . "&oauth_timestamp=" . $params["oauth_timestamp"]."&oauth_token=" . $params["oauth_token"] .
-	 "&oauth_version=" .$params["oauth_version"] . "&segment_index=" . $message["segment_index"]); 
+	 "&oauth_version=" .$params["oauth_version"]. "&segment_index=" . $message["segment_index"]); 
 
 	
 	// echo ("command=" . $message["command"] . "&media=" . $message["media"] . "&media_id="  .  $message["media_id"] . "&oauth_consumer_key=" . $params["oauth_consumer_key"]
@@ -85,16 +85,14 @@ function generateMediaSingatureAPPEND($request, $message, $params, $secrets){
 	
 	$base_string = ($request_method . "&" .  $base_url  . "&" . $paramter_string);
 			
+			echo $base_string . "<br/><br/>";
+			
 	$secret_string = $secrets["consumer_secret"] . "&" . $secrets["oauth_secret"];
-
-	$signature =  strtoupper(hash_hmac("SHA1",$base_string, $secret_string));	
+	echo $secret_string. "<br/><br/>";;
+	$signature =  urlencode(base64_encode(hash_hmac("SHA1",$base_string, $secret_string, true)));	
 	
-	$split_hex_signature = "";
-	foreach(str_split($signature, 2) as $split){
-		$split_hex_signature .= chr(hexdec($split));
-	}
-
-	$signature = urlEncodeCustom(base64_encode($split_hex_signature));
+	echo $signature . "<br>";
+	
 	return $signature;
 }
 function generateMediaSingatureCOMBINED($request, $message, $params, $secrets){	
@@ -113,6 +111,36 @@ function generateMediaSingatureCOMBINED($request, $message, $params, $secrets){
 	echo "$base_string<hr>";
 	$signature =  (hash_hmac("SHA1",$base_string, $secret_string, true));	
 	$signature = urlEncodeCustom(base64_encode($signature));
+	return $signature;
+}
+function generateMediaSingatureFINAL($request, $message, $params, $secrets){	
+  // BUILD SIGNATURE
+	$request_method = strtoupper($request["request_method"]);
+	$base_url = urlEncodeCustom($request["base_url"]);
+	//$message["media_data"] = 123;
+	$paramter_string = urlEncodeCustom("command=" . $message["command"] . "&media_id="  .  $message["media_id"] . "&oauth_consumer_key=" . $params["oauth_consumer_key"]
+	 . "&oauth_nonce=" . $params["oauth_nonce"] . "&oauth_signature_method=" . $params["oauth_signature_method"] . "&oauth_timestamp=" . $params["oauth_timestamp"]."&oauth_token=" . $params["oauth_token"] .
+	 "&oauth_version=" .$params["oauth_version"]); 
+
+	
+	// echo ("command=" . $message["command"] . "&media=" . $message["media"] . "&media_id="  .  $message["media_id"] . "&oauth_consumer_key=" . $params["oauth_consumer_key"]
+	 // . "&oauth_nonce=" . $params["oauth_nonce"] . "&oauth_signature_method=" . $params["oauth_signature_method"] . "&oauth_timestamp=" . $params["oauth_timestamp"]."&oauth_token=" . $params["oauth_token"] .
+	 // "&oauth_version=" .$params["oauth_version"] . "&segment_index=" . $message["segment_index"]) . "<br><br>"; 
+	
+	
+	// echo "<hr>" . $message["media"] . "<hr>";
+	
+	
+	$base_string = ($request_method . "&" .  $base_url  . "&" . $paramter_string);
+			
+			echo $base_string . "<br/><br/>";
+			
+	$secret_string = $secrets["consumer_secret"] . "&" . $secrets["oauth_secret"];
+	echo $secret_string. "<br/><br/>";;
+	$signature =  urlencode(base64_encode(hash_hmac("SHA1",$base_string, $secret_string, true)));	
+	
+	echo $signature . "<br>";
+	
 	return $signature;
 }
 function getMediaID($binary_file, $size, $mime){
@@ -134,13 +162,11 @@ function getMediaID($binary_file, $size, $mime){
 	$timestamp = time();
 	$access_token =  $keys["access_token"];
 	$oauth_version = "1.0";
-	
 	//Secrets
 	$consumer_secret = $keys["consumer_secret"];
 	$oauth_secret = $keys["oauth_secret"];
 	
-	
-			echo "<hr/>";
+			echo "<br/><br/>";
 	/////////////MAKE INIT////////////
 	//post data
 	$command = "INIT";
@@ -180,54 +206,57 @@ function getMediaID($binary_file, $size, $mime){
 "Authorization: OAuth oauth_consumer_key=\"$consumer_key\",oauth_nonce=\"$random_value\",oauth_signature=\"$signature\",oauth_signature_method=\"$method\",oauth_timestamp=\"$timestamp\",oauth_token=\"$access_token\",oauth_version=\"$oauth_version\""																				
 												);									
 	//request
+	
 	$curl = curl_init($media_api);
 	curl_setopt($curl, CURLOPT_POST, 1);
 	curl_setopt($curl, CURLOPT_HTTPHEADER, $header_data);
 	curl_setopt($curl, CURLOPT_POSTFIELDS, $postfield_string);
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-	print_r ($header_data) .  "++";
-	$media_id = json_decode(curl_exec($curl), true);
-	print_r ($media_id);
-	$media_id = $media_id["media_id_string"];
-	
-	
-			echo "<hr/>";
+	$media_id_arr = json_decode(curl_exec($curl), true);
+	print_r ($media_id_arr);
+	$media_id = $media_id_arr["media_id_string"];
+
+			echo "<br/><br/>";
 		/////////////MAKE APPEND////////////
 			//post data
 	$command = "APPEND";
 	$segment_index = 0;
-	$postfield_string = "command=$command&media_id=$media_id&segment_index=$segment_index&media=$binary_file";
-	$msg_len = (strlen($postfield_string));
+	
+	
 	//header data
 		  // BUILD SIGNATURE			
-		$signature =  generateMediaSingatureAppend(array(
-									"base_url" => $media_api,
-									"request_method" => "POST"
-									),
-								array(
-									"command" => "$command",
-									"media" => "$binary_file",
-									"media_id"=>"$media_id",
-									"segment_index"=>"$segment_index"
-									),			  
-								array(
-									"oauth_version" => "$oauth_version",
-									"oauth_nonce" => "$random_value",
-									"oauth_token" => "$access_token",
-									"oauth_timestamp" => "$timestamp",
-									"oauth_consumer_key" => "$consumer_key",
-									"oauth_signature_method" => "$method"
-									),
-								array(
-									"consumer_secret" => "$consumer_secret",
-									"oauth_secret" => "$oauth_secret"
-									)
-								);
+	$signature =  generateMediaSingatureAppend(array(
+								"base_url" => $media_api,
+								"request_method" => "POST"
+								),
+							array(
+								"command" => "$command",
+								"media" => "$binary_file",
+								"media_id"=>"$media_id",
+								"segment_index"=>"$segment_index"
+								),			  
+							array(
+								"oauth_version" => "$oauth_version",
+								"oauth_nonce" => "$random_value",
+								"oauth_token" => "$access_token",
+								"oauth_timestamp" => "$timestamp",
+								"oauth_consumer_key" => "$consumer_key",
+								"oauth_signature_method" => "$method"
+								),
+							array(
+								"consumer_secret" => "$consumer_secret",
+								"oauth_secret" => "$oauth_secret"
+								)
+							);
 									
-
-	
-	$header_data = array("Accept: */*", "Connection: close","User-Agent: VerniyXYZ-CURL" ,"Content-Transfer-Encoding: binary",
-												"Content-Type: multipart/form-data;charset=UTF-8", 
+	$postfield_string = "command=$command&media_id=$media_id&segment_index=$segment_index&media=$binary_file";
+	$postfield_string = "media=" . urlencode($binary_file) . "&oauth_consumer_key=$consumer_key&oauth_nonce=$random_value&oauth_signature=$signature&oauth_signature_method=$method&oauth_timestamp=$timestamp&oauth_token=$access_token&oauth_version=$oauth_version";
+						//&command=$command&media_id=$media_id&segment_index=$segment_index";
+			$postfield_string = "media=" . urlencode($binary_file) . "&command=$command&media_id=$media_id&segment_index=$segment_index" ;
+	$msg_len = (strlen($postfield_string));
+	echo $msg_len . "<br/>";
+	$header_data = array("Except:", "Connection: close","User-Agent: VerniyXYZ-CURL" ,"Content-Transfer-Encoding: binary",
+												"Content-Type: application/x-www-form-urlencoded", 
 												"Content-Length: $msg_len", "Host: upload.twitter.com",
 "Authorization: OAuth oauth_consumer_key=\"$consumer_key\",oauth_nonce=\"$random_value\",oauth_signature=\"$signature\",oauth_signature_method=\"$method\",oauth_timestamp=\"$timestamp\",oauth_token=\"$access_token\",oauth_version=\"$oauth_version\""																				
 												);									
@@ -236,14 +265,65 @@ function getMediaID($binary_file, $size, $mime){
 	curl_setopt($curl, CURLOPT_POST, 1);
 	curl_setopt($curl, CURLOPT_HTTPHEADER, $header_data);
 	curl_setopt($curl, CURLOPT_POSTFIELDS, $postfield_string);
+
+	echo  "<br/>";
+	
 	curl_setopt($curl, CURLOPT_HEADER  , true);  // we want headers
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-	print_r ($header_data) . "++";
+	$http_response = curl_exec($curl);
+	echo $http_response;
+
+		/////////////MAKE FINAL/
+		
+		$command = "FINALIZE";
+		
+		$signature =  generateMediaSingatureFINAL(array(
+								"base_url" => $media_api,
+								"request_method" => "POST"
+								),
+							array(
+								"command" => "$command",
+								"media" => "$binary_file",
+								"media_id"=>"$media_id",
+								"segment_index"=>"$segment_index"
+								),			  
+							array(
+								"oauth_version" => "$oauth_version",
+								"oauth_nonce" => "$random_value",
+								"oauth_token" => "$access_token",
+								"oauth_timestamp" => "$timestamp",
+								"oauth_consumer_key" => "$consumer_key",
+								"oauth_signature_method" => "$method"
+								),
+							array(
+								"consumer_secret" => "$consumer_secret",
+								"oauth_secret" => "$oauth_secret"
+								)
+							);
+		$postfield_string = "command=$command&media_id=$media_id" ;
+	$msg_len = (strlen($postfield_string));
+	echo $msg_len . "<br/>";
+	$header_data = array("Except:", "Connection: close","User-Agent: VerniyXYZ-CURL" ,"Content-Transfer-Encoding: binary",
+												"Content-Type: application/x-www-form-urlencoded", 
+												"Content-Length: $msg_len", "Host: upload.twitter.com",
+"Authorization: OAuth oauth_consumer_key=\"$consumer_key\",oauth_nonce=\"$random_value\",oauth_signature=\"$signature\",oauth_signature_method=\"$method\",oauth_timestamp=\"$timestamp\",oauth_token=\"$access_token\",oauth_version=\"$oauth_version\""																				
+												);									
+	//request
+	$curl = curl_init($media_api);
+	curl_setopt($curl, CURLOPT_POST, 1);
+	curl_setopt($curl, CURLOPT_HTTPHEADER, $header_data);
+	curl_setopt($curl, CURLOPT_POSTFIELDS, $postfield_string);
+
+	echo  "<br/>";
+	
+	curl_setopt($curl, CURLOPT_HEADER  , true);  // we want headers
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 	$http_response = curl_exec($curl);
 	echo $http_response;
 	
 			echo "<hr/>";
-		/////////////MAKE FINAL/
+			return 	$media_id ;
+	
 }
 function getMediaIDCOMBINED($binary_file, $size, $mime){
 	$media_api = "https://upload.twitter.com/1.1/media/upload.json";
@@ -313,7 +393,6 @@ function getMediaIDCOMBINED($binary_file, $size, $mime){
 	curl_setopt($curl, CURLOPT_POSTFIELDS, $postfield_string);
 	curl_setopt($curl, CURLOPT_HEADER  , true);  // we want headers
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-	print_r ($header_data) . "++";
 	$http_response = curl_exec($curl);
 	echo $http_response;
 	
@@ -337,104 +416,13 @@ for($file = 0 ; $file < 4 ; $file++){
 		$binary = file_get_contents($file_arr[$file]);
 		//$base64 = 'data:image/' . $type . ';base64,' . base64_encode($binary);
 		$base64 = base64_encode($binary);
-		echo $base64;
+		//echo $base64;
 		//upload file to twitter and get id for use in files?
 		$size = filesize($file_arr[$file]);
-		//$image_string = getMediaID($base64, $size, 'image/' . $type);
-		//getMediaIDCOMBINED($base64, $size, 'image/' . $type);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	//////////////////////////---------------------========================================----------------------------//////////////////////////////////////////	
-		
-		
-		
-		//authorization details
-$settings = fopen("settings/keys.ini","r");
-$keys = array();
-while(!feof($settings)){
-	$line = fgets($settings);
-	$key = substr($line,0,strpos($line, ":"));
-			//eat last character
-	$value = trim(substr($line, strpos($line, ":")+1));
-	$keys[$key] = $value;
-}
-$consumer_key = $keys["consumer_key"];
-$random_value = substr(( base64_encode(rand(1000000000,10000000000000)) . base64_encode(rand(1000000000,10000000000000)) . base64_encode(rand(1000000000000,100000000000000))), 0, 32);
-$random_value =  str_replace("/", "5", str_replace("=", "2", $random_value));
-$method = "HMAC-SHA1";
-$timestamp = time();
-$access_token =  $keys["access_token"];
-$oauth_version = "1.0";
-
-//Secrets
-$consumer_secret = $keys["consumer_secret"];
-$oauth_secret = $keys["oauth_secret"];
-		
-		
-		
-		
-		
-				//connect to twitter
-		$oTwitter = new TwitterOAuth($keys["consumer_key"], $consumer_secret, $access_token, $oauth_secret);
-		$oTwitter->host = "https://api.twitter.com/1.1/";
-
-		//make output visible in browser
-		if (!empty($_SERVER['HTTP_HOST'])) {
-			echo '<pre>';
-		}
-
-		//load args
-
-		define('DS', DIRECTORY_SEPARATOR);
-		
-		
-		
-				//upload image and save media id to attach to tweet
-        printf("Uploading to Twitter: %s\n", $file_arr[$file]);
-		$sImageBinary = base64_encode(file_get_contents($file_arr[$file]));
-		if ($sImageBinary && strlen($sImageBinary) < 15 * pow(1024, 2)) { //max size is 15MB
-
-			$oRet = $oTwitter->upload('media/upload', array('media' => $sImageBinary));
-			if (isset($oRet->errors)) {
-				printf('Twitter API call failed: media/upload (%s)', $oRet->errors[0]->message);
-				return FALSE;
-			} else {
-				printf("- uploaded %s to attach to next tweet\n", $file_arr[$file]);
-			}
-
-			print_r ($oRet);
-			$image_string .= $oRet->media_id_string;
-			if($file > 0 && $file < sizeof($file_arr) - 1) $image_string .= ","; 
-			
-        } else {
-            printf("- picture is too large!\n");
-		}
-
-
-
-
-	//////////////////////////---------------------========================================----------------------------//////////////////////////////////////////	
-		
-
-
-
-
-
-		
+		if($file == 0)
+			$image_string = getMediaID($base64, $size, 'image/' . $type);
+		else
+			$image_string .= "," . getMediaID($base64, $size, 'image/' . $type);		
 	}
 }
 
